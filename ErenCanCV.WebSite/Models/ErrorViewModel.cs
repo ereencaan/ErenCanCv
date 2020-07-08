17ab84ad0aa64917cff12e3db2e996a080d059d1
ereencaan <54391887+ereencaan@users.noreply.github.com>
2020-07-08 20:41:44 +0300

    Add project files.

---
+++ ErenCanCV.WebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ErenCanCV.WebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
